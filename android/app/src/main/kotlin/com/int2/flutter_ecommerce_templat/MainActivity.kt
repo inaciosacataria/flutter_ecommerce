b4ad01d54ddc75d8cd4947cae3c23514900ee909
inaciosacataria <54165291+inaciosacataria@users.noreply.github.com>
2022-11-24 16:51:25 +0200

    project added

---
+++ android/app/src/main/kotlin/com/int2/flutter_ecommerce_templat/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.int2.flutter_ecommerce_templat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/mainurl.dart
@@ -0,0 +1,6 @@
+class MainUrl {
+  String getMainUrl() {
+    String mainurl = "http://192.168.11.240/ecom/";
+    return mainurl;
+  }
+}
